68226cdc0faa4a0b857f1e75653e334bfa956288
eduardobastier93 <eduardobastier93@gmail.com>
2023-08-25 16:40:13 -0300

    Add function with return

--- calculo_imc.dart
+++ calculo_imc.dart
@@ -9,40 +9,44 @@ calculoImc(){
   String? textPeso = stdin.readLineSync();
   double?  peso = double.tryParse(textPeso!);
 
-if (peso != null){
+  if (peso != null && peso > 0){
   print("=== Digite sua altura ===");
   String? textAltura = stdin.readLineSync();
   double? altura = double.tryParse(textAltura!);
 
-if (altura != null){
-  double imc = peso / (altura * altura);  
+  if (altura != null && altura > 0){
+  double imc = calcImc(peso, altura);  
   imprimirResultado(imc);
   } else {
       print("Altura inválida.");
     }
   } else {
     print("Peso inválido.");
-  }
+ }
 }
 
-imprimirResultado(imc){
+double calcImc(double peso, double altura){
+  return peso / (altura * altura);
+}
+
+imprimirResultado(double imc){
     print("===========================");
 
   if (imc < 16){
-    print("Magreza grave");
+    print("Magreza grave.");
   } else if(imc >= 16 && imc <17){
-    print("Magreza moderada");
+    print("Magreza moderada.");
   } else if(imc >= 17 && imc <18.5){
-    print("Magreza leve");
+    print("Magreza leve.");
   } else if(imc >= 18.5 && imc < 25){
-    print("Peso normal");
+    print("Peso normal.");
   } else if(imc >= 25 && imc < 30){
-    print("Sobrepeso");
+    print("Sobrepeso.");
   } else if(imc >= 30 && imc < 35){
-    print("Obesidade grau 1");
+    print("Obesidade grau 1.");
   } else if(imc >= 35 && imc < 40){
-    print("Obesidade grau 2");
+    print("Obesidade grau 2.");
   } else {
-    print("Obesidade grau 3");
+    print("Obesidade grau 3.");
   }
 }
